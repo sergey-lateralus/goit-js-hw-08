586ae857f187d7039d594daf664e9f6d2ce9a003
sergey-lateralus <sergey.nezhyn@gmail.com>
2021-11-21 22:37:58 +0200

    Добавляет решение задания 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+
+const galleryMarkup = createMarkup(galleryItems);
+
+gallery.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+function createMarkup(array) {
+    return array.map(({ preview, original, description }) => {
+        return `<a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>`
+    }).join('');
+};
+
+const lightbox = new SimpleLightbox('.gallery a', { captionType: 'attr', captionsData: 'alt', captionDelay: 250 });
